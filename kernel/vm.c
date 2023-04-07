315b4cbb9f57d2be04acb9ec6f69469e1b69012c
Jun Zhang <jun@junz.org>
2023-04-07 21:01:32 +0800

    lab3: print the pagetable
    Signed-off-by: Jun Zhang <jun@junz.org>

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+static void print_indent(int level) {
+  for (int i = 0; i < level; ++i) {
+    printf(".");
+  }
+  printf(" ");
+}
+
+static void print_pagetable(pagetable_t pagetable, int level) {
+  for (int i = 0; i < 512; ++i) {
+    pte_t pte = pagetable[i];
+    uint64 child = PTE2PA(pte);
+    if (pte == 0) {
+      continue;
+    }
+    print_indent(level);
+    if ((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      print_pagetable((pagetable_t)child, level + 2);
+    } else {
+      printf("%d: pte %p pa %p\n", i, pte, child);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  print_pagetable(pagetable, 2);
+}
