e724ca70ddc01f0b61d8f52f0448d9f902c39d65
Jun Zhang <jun@junz.org>
2023-04-12 09:11:51 +0800

    lab4: implement backtrace
    Signed-off-by: Jun Zhang <jun@junz.org>

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,6 +121,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -131,4 +132,16 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void backtrace(void) {
+  uint64 fp = r_fp();
+  printf("%p\n", *(uint64*)(fp - 8));
+  fp = *(uint64*)(fp - 16);
+  // As long as we're still in a valid page.
+  while (PGROUNDUP(fp) - PGROUNDDOWN(fp) == 4096) {
+    printf("%p\n", *(uint64*)(fp - 8));
+    // next previous stack frame address.
+    fp = *(uint64*)(fp - 16);
+  }
 }
